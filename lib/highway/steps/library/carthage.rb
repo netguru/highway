39dc5ea88bfc1aa7c15e39e64d3270f73d4fd20f
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2018-12-10 20:09:27 +0100

    Add carthage step

---
+++ lib/highway/steps/library/carthage.rb
@@ -0,0 +1,70 @@
+#
+# carthage.rb
+# Copyright © 2018 Netguru S.A. All rights reserved.
+#
+
+require "highway/steps/infrastructure"
+
+module Highway
+  module Steps
+    module Library
+
+      # A step for executing Carthage.
+      class Carthage < Step
+
+        public
+
+        def self.name()
+          "carthage"
+        end
+
+        def self.parameters()
+          [
+            Parameter.new(
+              name: "command",
+              required: false,
+              type: Types::Enum.new("bootstrap", "update"),
+              default_value: "bootstrap"
+            ),
+            Parameter.new(
+              name: "github_token",
+              required: false,
+              type: Types::String.new(),
+            ),
+            Parameter.new(
+              name: "platforms",
+              required: true,
+              type: Types::Array.new(element_type: Types::Enum.new("iOS", "macOS", "tvOS", "watchOS")),
+            ),
+          ]
+        end
+
+        def self.run(parameters:, context:)
+
+          context.assert_executable_available!("carthage")
+          
+          command = parameters["command"]
+          platform = parameters["platforms"].join(",").gsub(%r(macOS), "Mac")
+          token = parameters["github_token"]
+
+          env = {
+            "GITHUB_ACCESS_TOKEN" => token
+          }
+
+          context.with_env(env) {
+            
+            context.run_action("carthage", options: {
+              cache_builds: true,
+              command: command,
+              platform: platform,
+            })
+
+          }
+
+        end
+
+      end
+
+    end
+  end
+end
