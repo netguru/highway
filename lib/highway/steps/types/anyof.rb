3b5983f95f13a2aabd835f3a28e2f15cb1101378
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2018-12-19 22:54:36 +0100

    Retain type tags in anyof type

--- lib/highway/steps/types/anyof.rb
+++ lib/highway/steps/types/anyof.rb
@@ -16,11 +16,39 @@ module Highway
 
         # Initialize an instance.
         #
-        # @param types [Object] Types. Order matters, first takes precedence.
+        # The first variadic argument accepts hashes containing `tag` and
+        # `type`. Tags are used to later denote which type the value falls to.
+        #
+        # Consider a step which defines the following parameter:
+        #
+        # ```
+        # Types::AnyOf(
+        #   {
+        #     tag: :channel,
+        #     type: Types::String.regex(/#\w+/)
+        #   },
+        #   {
+        #     tag: :user,
+        #     type: Types::String.regex(/@\w+/)
+        #   },
+        # )
+        # ```
+        #
+        # Later, when a step which defined such a parameter receives a value,
+        # it doesn't receive just a plain `String`, it receives:
+        #
+        # ```
+        # { tag: :channel, value: "#general" }
+        # ```
+        #
+        # Thus retaining the information about which type definition matched
+        # the value.
+        #
+        # @param type_defs [Array] Type definitions. First match wins.
         # @param validate [Proc] A custom value validation block.
-        def initialize(*types, validate: nil)
+        def initialize(*type_defs, validate: nil)
           super(validate: validate)
-          @types = types
+          @type_defs = type_defs
         end
 
         # Typecheck and coerce a value if possible.
@@ -32,7 +60,8 @@ module Highway
         #
         # @return [Object, nil]
         def typecheck(value)
-          @types.map { |type| typecheck_and_validate(value) }.find { |typechecked| typechecked != nil }
+          typechecked_defs = @type_defs.map { |type_def| {tag: typedef[:tag], value: typedef[:type].typecheck_and_validate(value) } }
+          typechecked_defs.find { |typechecked_def| typechecked_def[:value] != nil }
         end
 
       end
