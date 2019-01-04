f19a52e8d76857c8de5e7d01653e81a85c038248
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2019-01-04 01:01:04 +0100

    Generate detailed report in xcode-test step

--- lib/highway/steps/library/xcode_test.rb
+++ lib/highway/steps/library/xcode_test.rb
@@ -24,43 +24,83 @@ module Highway
               default_value: false,
             ),
             Parameter.new(
+              name: "configuration",
+              required: false,
+              type: Types::String.new(),
+            ),
+            Parameter.new(
+              name: "flags",
+              required: false,
+              type: Types::Array.new(Types::String.new()),
+              default_value: [],
+            ),
+            Parameter.new(
               name: "project",
               required: true,
               type: Types::AnyOf.new(
                 project: Types::String.regex(/.+\.xcodeproj/),
                 workspace: Types::String.regex(/.+\.xcworkspace/),
-              )
+              ),
             ),
             Parameter.new(
               name: "scheme",
               required: true,
               type: Types::String.new(),
             ),
+            Parameter.new(
+              name: "settings",
+              required: false,
+              type: Types::Dictionary.new(Types::String.new(), validate: lambda { |dict| dict.keys.all? { |key| /[A-Z_][A-Z0-9_]*/ =~ key } }),
+              default_value: {},
+            ),
           ]
         end
 
         def self.run(parameters:, context:, report:)
 
+          # Interpret the parameters. At this point they are parsed and
+          # transformed to be recognizable by Fastlane.
+
           clean = parameters["clean"]
+          configuration = parameters["configuration"]
           scheme = parameters["scheme"]
+
+          flags = parameters["flags"].join(" ")
+          settings = parameters["settings"].map { |setting, value| "#{setting}=\"#{value.shellescape}\"" }.join(" ")
+
+          xcargs = flags + settings
+          xcargs = nil if xcargs.empty?
 
           project_key = parameters["project"][:tag]
           project_value = parameters["project"][:value]
 
-          output_raw_temp_dir = report.prepare_artifact_temp_dir()
-          output_raw_path = report.prepare_artifact_file("raw.log")
+          # Prepare artifacts. Create temporary directories, get file names that
+          # will be later passed to the build command.
 
-          output_html_path = report.prepare_artifact_file("report.html")
-          output_junit_path = report.prepare_artifact_file("report.junit")
+          output_raw_temp_dir = Dir.mktmpdir()
+          output_raw_path = report.prepare_artifact("raw.log")
+
+          output_html_path = report.prepare_artifact("report.html")
+          output_junit_path = report.prepare_artifact("report.junit")
 
           output_html_file = File.basename(output_html_path)
           output_junit_file = File.basename(output_junit_path)
+
+          # Configure xcpretty. Set custom locations of report artifacts so that
+          # we can track them accurately.
 
           output_dir = context.artifacts_dir
           output_types = ["html", "junit"].join(",")
           output_files = [output_html_file, output_junit_file].join(",")
 
+          # Prepare temporary variables.
+
+          report_test = {}
+          report_artifacts = {}
+
           rescued_error = nil
+
+          # Run the build and test.
 
           begin
 
@@ -69,7 +109,9 @@ module Highway
               project_key => project_value,
 
               clean: clean,
+              configuration: configuration,
               scheme: scheme,
+              xcargs: xcargs,
 
               buildlog_path: output_raw_temp_dir,
               output_directory: output_dir,
@@ -80,28 +122,104 @@ module Highway
 
           rescue FastlaneCore::Interface::FastlaneBuildFailure => error
 
-            report[:test_result] = :compile_error
-            report[:test_error] = error
+            # A compile error occured. Save it to be re-raised later.
 
+            report_test[:result] = :error
             rescued_error = error
 
           rescue FastlaneCore::Interface::FastlaneTestFailure => error
 
-            report[:test_result] = :test_failure
-            report[:test_error] = error
+            # A test failure occured. Save it to be re-raised later.
 
+            report_test[:result] = :failure
             rescued_error = error
+
+          else
+
+            # Build succeeded!
+
+            report_test[:result] = :success
 
           end
 
+          # Now the real fun begins. Move the raw xcodebuild log from temporary
+          # directory to artifacts directory.
+
           output_raw_temp_path = Dir.glob(File.join(output_raw_temp_dir, "*.log")).first
-          FileUtils.mv(output_raw_temp_path, output_raw_path) if output_raw_temp_path != nil
+          FileUtils.mv(output_raw_temp_path, output_raw_path)
 
-          report[:artifact_raw_log] = output_raw_path if File.exist?(output_raw_path)
-          report[:artifact_html_report] = output_html_path if File.exist?(output_html_path)
-          report[:artifact_junit_report] = output_junit_file if File.exist?(output_junit_path)
+          # Save the artifact paths in the subreport.
 
-          # Re-raise the build/test failure error after report is prepared.
+          report_artifacts[:raw] = output_raw_path
+          report_artifacts[:html] = output_html_path
+          report_artifacts[:junit] = output_junit_path
+
+          # Load the raw log and pipe it through xcpretty with a JSON formatter.
+          # That will output a machine-readable information about everything
+          # that happened in the build.
+
+          xcpretty_json_formatter_path = context.run_sh("xcpretty-json-formatter", bundle_exec: true, silent: true)
+          temp_json_report_path = File.join(Dir.mktmpdir(), "report.json")
+
+          context.with_modified_env({"XCPRETTY_JSON_FILE_OUTPUT" => temp_json_report_path}) do
+            context.run_sh(["cat", output_raw_path, "| xcpretty --formatter", xcpretty_json_formatter_path], bundle_exec: true, silent: true)
+          end
+
+          # Load the build report and a JUnit report into memory.
+
+          junit_report = Scan::TestResultParser.new.parse_result(File.read(output_junit_path))
+          xcode_report = JSON.parse(File.read(temp_json_report_path))
+
+          # Extract test numbers from JUnit report.
+
+          report_test_count = {}
+
+          report_test_count[:all] = junit_report[:tests]
+          report_test_count[:failed] = junit_report[:failures]
+          report_test_count[:succeeded] = report_test_count[:all] - report_test_count[:failed]
+
+          report_test[:count] = report_test_count
+
+          # Extract compile errors from the build report.
+
+          report_test_errors = []
+
+          report_test_errors += xcode_report.fetch("file_missing_errors", []).map { |entry|
+            {location: File.basename(entry["file_path"]), reason: entry["reason"]}
+          }
+
+          report_test_errors += xcode_report.fetch("compile_errors", []).map { |entry|
+            {location: File.basename(entry["file_path"]), reason: entry["reason"]}
+          }
+
+          report_test_errors += xcode_report.fetch("undefined_symbols_errors", []).map { |entry|
+            {location: entry["symbol"], reason: entry["message"]}
+          }
+
+          report_test_errors += xcode_report.fetch("format_duplicate_symbols", []).map {
+            {location: nil, reason: entry["message"]}
+          }
+
+          report_test_errors += xcode_report.fetch("errors", []).map { |entry|
+            {location: nil, reason: entry}
+          }
+
+          report_test[:errors] = report_test_errors
+
+          # Extract test failures from the build report.
+
+          report_test_failures = xcode_report.fetch("tests_failures", []).values.flatten.map { |entry|
+            {location: entry["test_case"], reason: entry["reason"]}
+          }
+
+          report_test[:failures] = report_test_failures
+
+          # Save the test and artifacts subreports in the report.
+
+          report[:test] = report_test
+          report[:artifacts] = report_artifacts
+
+          # Re-raise the error after the report is finally prepared.
 
           raise rescued_error if rescued_error != nil
 
