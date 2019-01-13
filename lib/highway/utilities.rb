7332426afe7503d2bc328559783758e61a8c4a53
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2019-01-13 12:32:39 +0100

    Add keypath_to_s and recursive_contains utilities

--- lib/highway/utilities.rb
+++ lib/highway/utilities.rb
@@ -21,6 +21,31 @@ module Highway
       Hash[subject.map(&transform)]
     end
 
+    # Join keypath into a string.
+    #
+    # @param keypath [Array<String>] A keypath.
+    #
+    # @return [String]
+    def self.keypath_to_s(keypath)
+      keypath.join(".")
+    end
+
+    # Recursively check whether the subject includes an element.
+    #
+    # @param subject [Object] A haystack.
+    # @param element [Object] A needle.
+    #
+    # @return [Boolean]
+    def self.recursive_include?(subject, element)
+      if subject.is_a?(Hash)
+        recursive_include?(subject.values, element)
+      elsif subject.respond_to?(:any?)
+        subject.any? { |value| recursive_include?(value, element) }
+      else
+        subject == element
+      end
+    end
+
   end
 
 end
