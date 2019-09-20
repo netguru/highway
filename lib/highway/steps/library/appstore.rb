13f86ea7ce5b0db03ed648424c5ecee359c458c4
Piotr Sękara <piotr.sekara@gmail.com>
2019-09-20 15:39:56 +0200

    Added AppStore step

---
+++ lib/highway/steps/library/appstore.rb
@@ -0,0 +1,114 @@
+#
+# appstore.rb
+# Copyright © 2019 Netguru S.A. All rights reserved.
+#
+
+require "fastlane"
+
+require "highway/steps/infrastructure"
+
+module Highway
+    module Steps
+      module Library
+
+        class AppStoreStep < Step
+
+          def self.name
+            "appstore"
+          end
+
+          def self.parameters
+            [
+              Parameters::Single.new(
+                name: "username",
+                required: true,
+                type: Types::String.regex(/^\S+@\S+\.\S+$/)
+              ),
+              Parameters::Single.new(
+                name: "password",
+                required: true,
+                type: Types::String.new()
+              ),
+              Parameters::Single.new(
+                name: "session",
+                required: false,
+                type: Types::String.new()
+              ),
+              Parameters::Single.new(
+                name: "app_identifier",
+                required: true,
+                type: Types::String.new()
+              ),
+              Parameters::Single.new(
+                name: "metadata_path",
+                required: false,
+                type: Types::String.new()
+              ),
+              Parameters::Single.new(
+                name: "screenshots_path",
+                required: false,
+                type: Types::String.new()
+              ),
+              Parameters::Single.new(
+                name: "skip_app_version_update",
+                required: false,
+                default: false,
+                type: Types::Bool.new()
+              ),
+              Parameters::Single.new(
+                name: "submit_for_review",
+                required: false,
+                default: false,
+                type: Types::Bool.new()
+              ),
+              Parameters::Single.new(
+                name: "team_id",
+                required: false,
+                type: Types::String.new()
+              ),
+              Parameters::Single.new(
+                name: "team_name",
+                required: false,
+                type: Types::String.new()
+              )
+            ]
+          end
+
+          def self.run(parameters:, context:, report:)
+
+            username = parameters["username"]
+            password = parameters["password"]
+            session = parameters["session"]
+            app_identifier = parameters["app_identifier"]
+            metadata_path = parameters["metadata_path"]
+            screenshots_path = parameters["screenshots_path"]
+            skip_app_version_update = parameters["skip_app_version_update"]
+            submit_for_review = parameters["submit_for_review"]
+            team_id = parameters["team_id"]
+            team_name = parameters["team_name"]
+
+            env = {
+              "FASTLANE_PASSWORD" => password,
+              "FASTLANE_SESSION" => session
+            }
+
+            context.with_modified_env(env) {
+              context.run_action("upload_to_app_store", options: {
+                username: username,
+                app_identifier: app_identifier,
+                metadata_path: metadata_path,
+                screenshots_path: screenshots_path,
+                skip_app_version_update: skip_app_version_update,
+                submit_for_review: submit_for_review,
+                team_id: team_id,
+                team_name: team_name
+              })
+            }
+  
+          end
+
+        end
+
+      end
+    end
+end
