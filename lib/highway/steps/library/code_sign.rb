4895fa9b163788a21a84ccbb2d469a25c6a29644
Piotr Sękara <piotr.sekara@gmail.com>
2020-10-28 15:41:41 +0100

    Added keychain support for code signing

--- lib/highway/steps/library/code_sign.rb
+++ lib/highway/steps/library/code_sign.rb
@@ -28,17 +28,29 @@ module Highway
               required: true,
               type: Types::String.new()
             ),
+            Parameters::Single.new(
+              name: "keychain_name",
+              required: false,
+              type: Types::String.new()
+            ),
+            Parameters::Single.new(
+              name: "keychain_password",
+              required: false,
+              type: Types::String.new()
+            )
           ]
         end
 
         def self.run(parameters:, context:, report:)
             path = parameters["path"]
             passphrase = parameters["passphrase"]
+            keychain_name = parameters["keychain_name"]
+            keychain_password = parameters["keychain_password"]
 
             # First of all, check if file exist.
 
             unless File.exist?(path)
-                context.interface.fatal!("Provisioning archive '#{path}' does not exist.")
+                context.interface.fatal!("File '#{path}' does not exist.")
             end
 
             # Now prepare output directory and file.
@@ -61,17 +73,34 @@ module Highway
 
             zipped_files = Array[]
             Zip::File.open(output_file_path) do |zip_file|
-                zip_file.each do |file|
-                    file_path = File.join(output_temp_dir, file.name)
+              zip_file.each do |file|
+                  file_path = File.join(output_temp_dir, file.name)
+                  unless file_path.match?(/\.DS_Store|__MACOSX|(^|\/)\._/)
                     zip_file.extract(file, file_path) unless File.exist?(file_path)
                     zipped_files.push(file_path) unless File.directory?(file_path)
-                end
+                  end
+              end
             end
 
             # Filter paths to certificates and profiles.
             
             provisioning_profile_paths = zipped_files.grep(/.*\.mobileprovision/)
             certificates_paths = zipped_files.grep(/.*\.p12/)
+
+            # Prepare new keychain.
+
+            keychain_name = "highway.keychain-db" if keychain_name.nil?
+            keychain_password = passphrase if keychain_password.nil?
+            context.run_action("create_keychain", options: {
+              name: keychain_name,
+              password: keychain_password,
+              default_keychain: true,
+              unlock: true,
+              timeout: 72000,
+              lock_when_sleeps: true,
+              lock_after_timeout: true,
+              add_to_search_list: true
+            })
 
             # Install provisioning profiles.
 
@@ -86,12 +115,14 @@ module Highway
             certificates_paths.each do |path|
                 context.run_action("import_certificate", options: {
                     certificate_path: path,
-                    certificate_password: passphrase
+                    certificate_password: passphrase,
+                    keychain_name: keychain_name,
+                    keychain_password: keychain_password
                 })
             end
         end
-      end
 
+      end
     end
   end
 end
